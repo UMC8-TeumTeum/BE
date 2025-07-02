ced7567428695b012172d8234856d0c90060ebd6
hyeonda02 <alceste951005@naver.com>
2025-07-02 10:58:06 +0900

    Chore : 초기 프로젝트 세팅 프로젝트 세팅 및 기본 dependencies 추가

---
+++ src/test/java/umc/teumteum/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package umc.teumteum.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
