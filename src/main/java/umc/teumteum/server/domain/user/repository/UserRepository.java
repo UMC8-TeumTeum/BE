27a68a81c497b4e2c0ca05a50397f6ed3de6ff06
yangjiae12 <yangjiae12@gmail.com>
2025-07-15 02:38:51 +0900

    Chore: 잘못 생성된 TeumRepository 파일 제거

--- src/main/java/umc/teumteum/server/domain/user/repository/UserRepository.java
+++ src/main/java/umc/teumteum/server/domain/user/repository/UserRepository.java
@@ -1,4 +1,8 @@
 package umc.teumteum.server.domain.user.repository;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import umc.teumteum.server.domain.user.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    // 필요시 커스텀 메서드 추가
 }
