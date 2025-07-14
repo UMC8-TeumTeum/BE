6546547699cadd0619ee40c1812d064745581763
Jinseong <jinsung528@gmail.com>
2025-07-14 21:17:48 +0900

    Refactor : Agreement, RemindAlarm Unique 제약 추가

--- src/main/java/umc/teumteum/server/domain/user/entity/RemindAlarm.java
+++ src/main/java/umc/teumteum/server/domain/user/entity/RemindAlarm.java
@@ -20,7 +20,7 @@ public class RemindAlarm extends BaseEntity {
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id", nullable = false)
+    @JoinColumn(name = "user_id", nullable = false, unique = true)
     private User user;
 
     @Column(name = "minutes_before", nullable = false)
