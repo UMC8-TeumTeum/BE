3eb4263e639171944c4b66a19592a55ade945ea9
yangjiae12 <yangjiae12@gmail.com>
2025-07-15 16:37:30 +0900

    Refactor: 팔로잉 목록 응답 DTO 변환 로직 FriendConverter로 분리
    - Friend → FollowingUserResponseDto 변환 책임을 컨버터로 분리
    - toFollowingUserResponse / toFollowingUserResponseList 메서드 구현

--- src/main/java/umc/teumteum/server/domain/friend/converter/FriendConverter.java
+++ src/main/java/umc/teumteum/server/domain/friend/converter/FriendConverter.java
@@ -1,5 +1,29 @@
 package umc.teumteum.server.domain.friend.converter;
 
+import umc.teumteum.server.domain.friend.dto.FollowingUserResponseDto;
+import umc.teumteum.server.domain.friend.entity.Friend;
+import umc.teumteum.server.domain.user.entity.User;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class FriendConverter {
-    // TODO : 변환 로직 추후 구현
+
+    public static FollowingUserResponseDto toFollowingUserResponse(Friend friend) {
+        User following = friend.getFollowing();
+        String imageUrl = following.getProfileImageUrl(); // key → url 매핑
+
+        return new FollowingUserResponseDto(
+                following.getId(),
+                following.getNickname(),
+                imageUrl,
+                friend.getIsFavorite()
+        );
+    }
+
+    public static List<FollowingUserResponseDto> toFollowingUserResponseList(List<Friend> friendList) {
+        return friendList.stream()
+                .map(FriendConverter::toFollowingUserResponse)
+                .collect(Collectors.toList());
+    }
 }
