2c254c7df67e394f742e9e4ffb74970d8d739561
yangjiae12 <yangjiae12@gmail.com>
2025-07-15 17:03:00 +0900

    Refactor: 팔로워 목록 응답 DTO 변환 로직 FriendConverter로 분리

--- src/main/java/umc/teumteum/server/domain/friend/converter/FriendConverter.java
+++ src/main/java/umc/teumteum/server/domain/friend/converter/FriendConverter.java
@@ -1,5 +1,6 @@
 package umc.teumteum.server.domain.friend.converter;
 
+import umc.teumteum.server.domain.friend.dto.FollowerUserResponseDto;
 import umc.teumteum.server.domain.friend.dto.FollowingUserResponseDto;
 import umc.teumteum.server.domain.friend.entity.Friend;
 import umc.teumteum.server.domain.user.entity.User;
@@ -26,4 +27,22 @@ public class FriendConverter {
                 .map(FriendConverter::toFollowingUserResponse)
                 .collect(Collectors.toList());
     }
+
+    public static FollowerUserResponseDto toFollowerUserResponse(Friend friend) {
+        User follower = friend.getFollower();
+        String imageUrl = follower.getProfileImageUrl();
+
+        return new FollowerUserResponseDto(
+                follower.getId(),
+                follower.getNickname(),
+                imageUrl
+        );
+    }
+
+    public static List<FollowerUserResponseDto> toFollowerUserResponseList(List<Friend> friendList) {
+        return friendList.stream()
+                .map(FriendConverter::toFollowerUserResponse)
+                .collect(Collectors.toList());
+    }
+
 }
