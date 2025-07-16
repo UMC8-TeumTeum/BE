1ecf4cb2fe4d376556f66f888d49ff541f9ef535
yangjiae12 <yangjiae12@gmail.com>
2025-07-17 00:07:34 +0900

    Refactor: 응답 상태 enum 값 SUGGESTED에서 RESEND로 변경
    - 재요청 흐름에 더 직관적인 이름으로 정비

--- src/main/java/umc/teumteum/server/domain/teum/entity/enums/ResponseStatus.java
+++ src/main/java/umc/teumteum/server/domain/teum/entity/enums/ResponseStatus.java
@@ -4,5 +4,5 @@ public enum ResponseStatus {
     PENDING,    // 수신자가 응답을 하지 않음
     ACCEPTED,   // 수신자가 요청을 수락
     REJECTED,   // 수신자가 요청을 거절
-    SUGGESTED   // 수신자가 시간대 바꿔 재요청
+    RESEND   // 수신자가 시간대 바꿔 재요청
 }
