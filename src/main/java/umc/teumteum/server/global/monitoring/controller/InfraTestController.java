4c76c9271513d46fdd7f63e59f4173fdcc53b62c
hyeonda02 <alceste951005@naver.com>
2025-07-16 16:25:47 +0900

    Feat : S3 연동 설정 및 테스트 API 추가 - properties 파일 수정 - S3Config 클래스 생성 및 S3Client 빈 등록 - S3 연결 테스트용 test api 작성

--- src/main/java/umc/teumteum/server/global/monitoring/controller/InfraTestController.java
+++ src/main/java/umc/teumteum/server/global/monitoring/controller/InfraTestController.java
@@ -3,6 +3,7 @@ package umc.teumteum.server.global.monitoring.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.List;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -11,8 +12,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import software.amazon.awssdk.services.s3.S3Client;
 import umc.teumteum.server.global.apiPayload.ApiResponse;
 import umc.teumteum.server.global.monitoring.service.InfraRedisService;
+import umc.teumteum.server.global.monitoring.service.InfraS3Service;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,6 +23,7 @@ import umc.teumteum.server.global.monitoring.service.InfraRedisService;
 @Tag(name = "Test", description = "Redis 등 인프라 점검을 위한 테스트 API 입니다.")
 public class InfraTestController {
   private final InfraRedisService infraRedisServiceImpl;
+  private final InfraS3Service infraS3ServiceImpl;
 
   @GetMapping("/redis/check")
   @Operation(summary = "Redis DB 분리 확인", description = "RT, 알림, AI 콘텐츠용 RedisTemplate 각각에 test_key를 저장/조회/삭제하여 DB 분리 여부를 확인합니다.")
@@ -57,6 +61,17 @@ public class InfraTestController {
   public ResponseEntity<ApiResponse> getPingRtRedisTemplate() {
     String pong = infraRedisServiceImpl.pingRtRedisTemplate();
     return ResponseEntity.ok(ApiResponse.onSuccess("Redis PING with rtTemplate : " + pong));
+  }
+
+
+  @GetMapping("/s3/list")
+  @Operation(
+      summary = "S3 연결 테스트",
+      description = "버킷에 정상적으로 접근되는지 확인하고, 지정한 prefix의 객체 리스트를 조회합니다."
+  )
+  public ResponseEntity<ApiResponse> listS3Objects() {
+    List<String> objects = infraS3ServiceImpl.listObjects();
+    return ResponseEntity.ok(ApiResponse.onSuccess(objects));
   }
 
 
---
+++ src/main/java/umc/teumteum/server/global/monitoring/service/InfraS3Service.java
@@ -0,0 +1,8 @@
+package umc.teumteum.server.global.monitoring.service;
+
+import java.util.List;
+
+public interface InfraS3Service {
+  public List<String> listObjects();
+
+}
