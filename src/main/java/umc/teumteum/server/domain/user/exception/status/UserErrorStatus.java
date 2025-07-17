86d71490882e2a1f258bbc1b3afb592306b77625
yangjiae12 <yangjiae12@gmail.com>
2025-07-17 20:07:45 +0900

    Feat: 사용자 성공/실패 상태 코드 Enum 정의
    - USER_NOT_FOUND, _USER_FOUND 상태 정의
    - ApiResponse 및 예외 처리용으로 사용

---
+++ src/main/java/umc/teumteum/server/domain/user/exception/status/UserErrorStatus.java
@@ -0,0 +1,37 @@
+package umc.teumteum.server.domain.user.exception.status;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import umc.teumteum.server.global.apiPayload.code.BaseErrorCode;
+import umc.teumteum.server.global.apiPayload.code.ErrorReasonDto;
+
+@Getter
+@AllArgsConstructor
+public enum UserErrorStatus implements BaseErrorCode {
+
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "USER4040", "존재하지 않는 사용자입니다.");
+
+    private final HttpStatus httpStatus;
+    private final String code;
+    private final String message;
+
+    @Override
+    public ErrorReasonDto getReason() {
+        return ErrorReasonDto.builder()
+                .isSuccess(false)
+                .code(code)
+                .message(message)
+                .build();
+    }
+
+    @Override
+    public ErrorReasonDto getReasonHttpStatus() {
+        return ErrorReasonDto.builder()
+                .httpStatus(httpStatus)
+                .isSuccess(false)
+                .code(code)
+                .message(message)
+                .build();
+    }
+}
---
+++ src/main/java/umc/teumteum/server/domain/user/exception/status/UserSuccessStatus.java
@@ -0,0 +1,37 @@
+package umc.teumteum.server.domain.user.exception.status;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import umc.teumteum.server.global.apiPayload.code.BaseCode;
+import umc.teumteum.server.global.apiPayload.code.ReasonDto;
+
+@Getter
+@AllArgsConstructor
+public enum UserSuccessStatus implements BaseCode {
+
+    _USER_FOUND(HttpStatus.OK, "USER2000", "사용자 조회 성공");
+
+    private final HttpStatus httpStatus;
+    private final String code;
+    private final String message;
+
+    @Override
+    public ReasonDto getReason() {
+        return ReasonDto.builder()
+                .isSuccess(true)
+                .code(code)
+                .message(message)
+                .build();
+    }
+
+    @Override
+    public ReasonDto getReasonHttpStatus() {
+        return ReasonDto.builder()
+                .httpStatus(httpStatus)
+                .isSuccess(true)
+                .code(code)
+                .message(message)
+                .build();
+    }
+}
