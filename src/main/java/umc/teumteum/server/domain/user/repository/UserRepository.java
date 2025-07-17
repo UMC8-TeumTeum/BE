3a040ae1e56ad08b8eb72beed521925bbdaf2b92
yangjiae12 <yangjiae12@gmail.com>
2025-07-17 20:07:21 +0900

    Feat: 닉네임 기반 사용자 조회 쿼리 추가
    - findByNickname(String nickname) 메서드 정의

--- src/main/java/umc/teumteum/server/domain/user/repository/UserRepository.java
+++ src/main/java/umc/teumteum/server/domain/user/repository/UserRepository.java
@@ -8,4 +8,6 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findBySocialTypeAndSocialId(SocialType socialType, String socialId);
+
+    Optional<User> findByNickname(String nickname);
 }
