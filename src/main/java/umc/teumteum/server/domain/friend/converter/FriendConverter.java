a72ce0636e94022390536ce5ee4a32d1f32eb8e6
yangjiae12 <yangjiae12@gmail.com>
2025-07-18 11:01:33 +0900

    Feat: 친구 프로필 응답 변환 메서드 추가
    - targetUser와 Friend 엔티티를 기반으로 FriendProfileResponseDto를 생성

--- src/main/java/umc/teumteum/server/domain/friend/converter/FriendConverter.java
+++ src/main/java/umc/teumteum/server/domain/friend/converter/FriendConverter.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import umc.teumteum.server.domain.friend.dto.FollowerUserResponseDto;
 import umc.teumteum.server.domain.friend.dto.FollowingUserResponseDto;
+import umc.teumteum.server.domain.friend.dto.FriendProfileResponseDto;
 import umc.teumteum.server.domain.friend.entity.Friend;
 import umc.teumteum.server.domain.user.entity.User;
 import umc.teumteum.server.global.util.S3Util;
@@ -51,4 +52,18 @@ public class FriendConverter {
                 .map(this::toFollowerUserResponse)
                 .collect(Collectors.toList());
     }
+
+    public FriendProfileResponseDto toFriendProfileResponse(User targetUser, Friend followRelation) {
+        String imageUrl = s3Util.toUrl(targetUser.getProfileImageKey());
+
+        return FriendProfileResponseDto.builder()
+                .userId(targetUser.getId())
+                .name(targetUser.getNickname())
+                .profileImageUrl(imageUrl)
+                .field(targetUser.getJob()) // 또는 getField() 등
+                .isFollowing(followRelation != null)
+                .isFavorite(followRelation != null && followRelation.getIsFavorite())
+                .build();
+    }
+
 }
