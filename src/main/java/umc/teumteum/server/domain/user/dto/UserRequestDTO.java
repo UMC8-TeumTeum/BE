2079bd55734d2e6c0585399ba70e9fab7affe2bc
Jinseong <jinsung528@gmail.com>
2025-07-23 01:46:42 +0900

    Refactor : AgreeRequest의 @NotNull에 message 추가

--- src/main/java/umc/teumteum/server/domain/user/dto/UserRequestDTO.java
+++ src/main/java/umc/teumteum/server/domain/user/dto/UserRequestDTO.java
@@ -18,19 +18,19 @@ public class UserRequestDTO {
     @AllArgsConstructor
     public static class AgreeRequest {
 
-        @NotNull
+        @NotNull(message = "서비스 이용약관 동의 여부를 입력해야 합니다")
         @Schema(description = "서비스 이용약관 동의 여부 (필수)", example = "true")
         private Boolean tosConsent;
 
-        @NotNull
+        @NotNull(message = "개인정보 수집 및 이용 동의 여부를 입력해야 합니다")
         @Schema(description = "개인정보 수집 및 이용 동의 여부 (필수)", example = "true")
         private Boolean privacyConsent;
 
-        @NotNull
+        @NotNull(message = "개인정보 제3자 제공 동의 여부를 입력해야 합니다")
         @Schema(description = "개인정보 제3자 제공 동의 여부 (선택)", example = "false")
         private Boolean thirdPartyConsent;
 
-        @NotNull
+        @NotNull(message = "마케팅 정보 수신 동의 여부를 입력해야 합니다")
         @Schema(description = "마케팅 정보 수신 동의 여부 (선택)", example = "false")
         private Boolean marketingConsent;
     }
