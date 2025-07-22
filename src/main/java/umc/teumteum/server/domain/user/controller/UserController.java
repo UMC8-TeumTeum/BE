dc5081c46e2dc1a76177fc7eb5d164ea0e6c4243
Jinseong <jinsung528@gmail.com>
2025-07-22 09:26:49 +0900

    Refactor : 약관 동의 API 엔드포인트 변경

--- src/main/java/umc/teumteum/server/domain/user/controller/UserController.java
+++ src/main/java/umc/teumteum/server/domain/user/controller/UserController.java
@@ -31,7 +31,7 @@ public class UserController {
             summary = "온보딩 약관 동의",
             description = "온보딩 과정에서 약관 동의 정보를 저장합니다."
     )
-    @PostMapping(value = "/onboarding/terms", produces = "application/json")
+    @PostMapping(value = "/onboarding/agreement", produces = "application/json")
     public ApiResponse<Object> saveTerms(
     ) {
         // TODO: 온보딩 약관 동의 저장 로직 구현
