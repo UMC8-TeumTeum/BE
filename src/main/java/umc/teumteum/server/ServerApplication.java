4f8aa5e19bdafaddafe7b216b99b7ec61cd484c5
hyeonda02 <alceste951005@naver.com>
2025-07-27 03:57:04 +0900

    Chore : 애플리케이션 전역 타임존을 Asia/Seoul로 설정

--- src/main/java/umc/teumteum/server/ServerApplication.java
+++ src/main/java/umc/teumteum/server/ServerApplication.java
@@ -1,5 +1,6 @@
 package umc.teumteum.server;
 
+import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -11,6 +12,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class ServerApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 		SpringApplication.run(ServerApplication.class, args);
 	}
 
