d206b0f4ce56dcc8c572eecb3a041e727419dfa1
yangjiae12 <yangjiae12@gmail.com>
2025-07-28 22:44:06 +0900

    Fix: 자신이 받은 틈 요청도 중복 충돌로 처리되던 문제 수정
    - validateTeum 로직에서 수신자로서 받은 요청까지 포함되어 시간 충돌 예외가 발생하던 문제 해결
    - teumRequestRepository.findTeumRequestsByUserAndDate 쿼리를 수정하여,
      사용자 본인이 생성한 틈 요청만 검증 대상이 되도록 변경

--- src/main/java/umc/teumteum/server/domain/teum/repository/TeumRequestRepository.java
+++ src/main/java/umc/teumteum/server/domain/teum/repository/TeumRequestRepository.java
@@ -19,16 +19,15 @@ public interface TeumRequestRepository extends JpaRepository<TeumRequest, Long> 
 
     /**
      * 다음 조건을 모두 만족하는 TeumRequest들을 조회:
-     * - 요청자 또는 수신자가 해당 사용자(user)인 요청
+     * - 요청자가 해당 사용자(user)인 요청
      * - 상태가 ACTIVE이고 아직 약속된 스케줄이 없음 (schedules.isEmpty)
      * - 요청 날짜(date)가 지정한 날짜와 일치
      * → 시간 겹침 여부는 자바에서 별도 검사
      */
     @Query("""
     SELECT tr FROM TeumRequest tr
-    LEFT JOIN tr.teumResponses r
     WHERE tr.status = 'ACTIVE'
-      AND (tr.user = :user OR r.receiverUser = :user)
+      AND tr.user = :user
       AND tr.schedules IS EMPTY
       AND tr.date = :date
 """)
