0148e783d81dd2f514a0065975d0cc0e41c26bf5
hyeonda02 <alceste951005@naver.com>
2025-08-06 00:00:02 +0900

    Fix : entrypoint.sh 포맷 및 실행 오류 수정

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
-f [[ -n "$OPEN_API_KEY_BASE64" ]]; then
+#!/bin/bash
+
+if [[ -n "$OPEN_API_KEY_BASE64" ]]; then
   export OPEN_API_KEY=$(echo "$OPEN_API_KEY_BASE64" | base64 -d)
 fi
 
-# Run the app
 exec java -Duser.timezone=Asia/Seoul -Dspring.profiles.active=prod -jar /app.jar
