ac4f03ed109c6c16441a0bc339dbc2c26980a066
hyeonda02 <alceste951005@naver.com>
2025-08-11 12:28:27 +0900

    Refactor : 채움활동 ai 컨텐츠 추천 API 수정

---
+++ src/main/java/umc/teumteum/server/domain/home/entity/enums/LocationType.java
@@ -0,0 +1,26 @@
+package umc.teumteum.server.domain.home.entity.enums;
+
+public enum LocationType {
+  HOUSE(1, "집"),       // 1, 집
+  SCHOOL(2, "학교"),    // 2, 학교
+  MINUTES_30(3, "30m"), // 3, 회사
+  HOUR_1(4, "1h"),      // 4, 이동중
+  OUTDOOR(5, "실외"),   // 5, 실외
+  INDOOR(6, "실내");    // 6, 실내
+
+  private final int locationId;
+  private final String displayName;
+
+  LocationType(int locationId, String displayName) {
+    this.locationId = locationId;
+    this.displayName = displayName;
+  }
+
+  public int getLocationId() {
+    return locationId;
+  }
+
+  public String getDisplayName() {
+    return displayName;
+  }
+}
