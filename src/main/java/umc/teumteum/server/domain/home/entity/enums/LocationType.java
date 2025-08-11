a7bdc67b209cf4034684fa2405d377103d49ef17
hyeonda02 <alceste951005@naver.com>
2025-08-11 16:21:55 +0900

    Refactor : LocationType 수정

--- src/main/java/umc/teumteum/server/domain/home/entity/enums/LocationType.java
+++ src/main/java/umc/teumteum/server/domain/home/entity/enums/LocationType.java
@@ -3,8 +3,8 @@ package umc.teumteum.server.domain.home.entity.enums;
 public enum LocationType {
   HOUSE(1, "집"),       // 1, 집
   SCHOOL(2, "학교"),    // 2, 학교
-  MINUTES_30(3, "회사"), // 3, 회사
-  HOUR_1(4, "이동중"),      // 4, 이동중
+  WORK(3, "회사"), // 3, 회사
+  ING(4, "이동중"),      // 4, 이동중
   OUTDOOR(5, "실외"),   // 5, 실외
   INDOOR(6, "실내");    // 6, 실내
 
