c4c092c7d605fc3411fed4aac32eea71e00ba76f
yangjiae12 <yangjiae12@gmail.com>
2025-10-24 16:34:48 +0900

    Feat: 신고 기능 엔티티 및 모델 정의
    - Report 엔티티: User, TeumRequest와의 관계 설정
    - ReportReason 엔티티: 신고 사유의 제목과 내용을 정의
    - ReportStatus, TargetType Enum 정의 및 Report 엔티티에 적용

---
+++ src/main/java/umc/teumteum/server/domain/report/entity/Report.java
@@ -0,0 +1,53 @@
+package umc.teumteum.server.domain.report.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import umc.teumteum.server.domain.report.entity.enums.ReportStatus;
+import umc.teumteum.server.domain.report.entity.enums.TargetType;
+import umc.teumteum.server.domain.teum.entity.TeumRequest;
+import umc.teumteum.server.domain.user.entity.User;
+import umc.teumteum.server.global.common.BaseEntity;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "report")
+public class Report extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "reporter_id", nullable = false)
+    private User reporter;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "target_user_id", nullable = true)
+    private User targetUser;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "target_request_id", nullable = true)
+    private TeumRequest teumRequest;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TargetType targetType;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "reason_id", nullable = false)
+    private ReportReason reason;
+
+    @Column(name = "other_reason_text", columnDefinition = "TEXT", nullable = true)
+    private String otherReason;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private ReportStatus status;
+
+}
---
+++ src/main/java/umc/teumteum/server/domain/report/entity/enums/ReportStatus.java
@@ -0,0 +1,8 @@
+package umc.teumteum.server.domain.report.entity.enums;
+
+public enum ReportStatus {
+    OPEN,       // 신고 오픈
+    REVIEWING,  // 검토 중
+    RESOLVED,   // 처리 완료
+    DISMISSED   // 처리 거부
+}
