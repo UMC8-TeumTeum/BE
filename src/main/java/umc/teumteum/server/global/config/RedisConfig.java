b63e020c65d96aacb668f8cd7dbf6df2612ae52e
Jinseong <jinsung528@gmail.com>
2025-11-04 14:56:40 +0900

    Chore : RedissonClient Bean 추가

--- src/main/java/umc/teumteum/server/global/config/RedisConfig.java
+++ src/main/java/umc/teumteum/server/global/config/RedisConfig.java
@@ -1,5 +1,8 @@
 package umc.teumteum.server.global.config;
 
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -69,5 +72,14 @@ public class RedisConfig {
   @Bean
   public RedisTemplate<String, String> profileImageRedisTemplate() {
     return createRedisTemplate(createConnectionFactory(4));
+  }
+
+  @Bean
+  public RedissonClient redissonClient() {
+    Config config = new Config();
+    config.useSingleServer()
+            .setAddress("redis://" + host + ":" + port)
+            .setPassword(password);
+    return Redisson.create(config);
   }
 }
