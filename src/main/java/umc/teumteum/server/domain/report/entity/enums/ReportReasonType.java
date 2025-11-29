82f2da8db1acb4e2afd8138632e1384bdc933986
yangjiae12 <yangjiae12@gmail.com>
2025-11-30 02:30:44 +0900

    refactor: 신고 사유 Enum 정의 및 엔티티 적용
    - ReportReason 엔티티에 reason_type 필드 매핑
    - isOther() 로직을 Enum 타입 비교 방식으로 확정

---
+++ src/main/java/umc/teumteum/server/domain/report/entity/enums/ReportReasonType.java
@@ -0,0 +1,11 @@
+package umc.teumteum.server.domain.report.entity.enums;
+
+public enum ReportReasonType {
+    ABUSE,      // 혐오발언
+    SEXUAL,     // 성희롱
+    FRAUD,      // 거짓정보
+    ILLEGAL,    // 유해콘텐츠
+    PRIVACY,    // 개인정보
+    SPAM,       // 스팸
+    OTHER       // 기타
+}
