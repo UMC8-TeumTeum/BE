c97a52889272ac05e42df17d05d1b531a82ce440
Jinseong01 <jinsung528@gmail.com>
2025-12-15 01:41:05 +0900

    Refactor: AsyncConfig 보완 및 누락된 @EnableAsync 어노테이션 추가

--- src/main/java/umc/teumteum/server/global/config/AsyncConfig.java
+++ src/main/java/umc/teumteum/server/global/config/AsyncConfig.java
@@ -16,8 +16,10 @@ public class AsyncConfig implements AsyncConfigurer {
         executor.setCorePoolSize(5);       // 최소 스레드 수
         executor.setMaxPoolSize(10);       // 최대 스레드 수
         executor.setQueueCapacity(30);     // 대기열 크기
-        executor.setThreadNamePrefix("EVENT-");
+        executor.setThreadNamePrefix("EVENT-");     // 스레드 이름 접두사
         executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());    // 거부 정책: 스레드 풀이 꽉 차면 호출 스레드에서 동기 실행
+        executor.setWaitForTasksToCompleteOnShutdown(true);     // 애플리케이션 종료 시, 대기 중인 작업이 끝날 때까지 기다림
+        executor.setAwaitTerminationSeconds(10);                // 최대 대기 시간: 종료 시 최대 10초까지 기다림
         executor.initialize();
         return executor;
     }
