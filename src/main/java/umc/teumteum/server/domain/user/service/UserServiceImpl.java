f0faaa4a5b9f05f2d6486ea78d3ede91cae04680
sunninz <tnals00718@gmail.com>
2026-01-16 16:08:34 +0900

    Fix : 익명화가 되지 않던 오류 수정

--- src/main/java/umc/teumteum/server/domain/user/service/UserServiceImpl.java
+++ src/main/java/umc/teumteum/server/domain/user/service/UserServiceImpl.java
@@ -554,14 +554,14 @@ public class UserServiceImpl implements UserService {
         User u = userRepository.findById(user.getId())
                 .orElseThrow(()-> new UserException(UserErrorStatus.USER_NOT_FOUND));
 
-        // 1. 유저 관련 데이터 삭제
+        // 1. 유저 익명화
+        u.withdraw();
+
+        // 2. 유저 관련 데이터 삭제
         userDataCleaner.clean(u.getId());
 
-        // 2. 프로필 이미지 삭제
+        // 3. 프로필 이미지 삭제
         deleteUserImage(u);
-
-        // 3. 유저 익명화
-        u.withdraw();
     }
 
     // 수면패턴 삭제
